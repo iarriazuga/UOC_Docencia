d7d82c694b9e9ac66884b3312f0b8680e9953f8c
iarriazuga <iarriazuga@uoc.edu>
2024-11-25 16:16:46 +0100

    transform

--- Transform/Models/F_DADES_ACADEMIQUES_DIMAX.sql
+++ Transform/Models/F_DADES_ACADEMIQUES_DIMAX.sql
@@ -6,18 +6,27 @@
 
 CREATE OR REPLACE TABLE DB_UOC_PROD.DDP_DOCENCIA.F_DADES_ACADEMIQUES_DIMAX AS  -- DDP_DOCENCIA
 
-with node_structure_aplanation AS ( 
-SELECT 
-    db_uoc_prod.stg_dadesra.dimax_item_dimax.cami_node,
-    -- db_uoc_prod.stg_dadesra.dimax_resofite_path.id_resource,
-    db_uoc_prod.stg_dadesra.dimax_resofite_path.node_cami as id_resource,  -- id_ que equivale a la tabla de dim_catalog 
-    db_uoc_prod.stg_dadesra.dimax_resofite_path.node_cami,
-    db_uoc_prod.stg_dadesra.dimax_resofite_path.node_recurs, -- same as  db_uoc_prod.stg_dadesra.dimax_item_dimax.id 
-    -- db_uoc_prod.stg_dadesra.dimax_resofite_path.ordre,
-    db_uoc_prod.stg_dadesra.dimax_item_dimax.titol,
-    db_uoc_prod.stg_dadesra.dimax_v_recurs.id_recurs AS id_recurs2,
-    db_uoc_prod.stg_dadesra.dimax_v_recurs.titol AS titol_resource,
-    SUBSTR(db_uoc_prod.stg_dadesra.dimax_item_dimax.titol, 0, 6) AS DIM_ASSIGNATURA_KEY
+with dimax_resofite_path_unified AS (  
+    select  distinct
+        db_uoc_prod.stg_dadesra.dimax_resofite_path.node_recurs,  
+        db_uoc_prod.stg_dadesra.dimax_resofite_path.node_cami,
+        -- db_uoc_prod.stg_dadesra.dimax_resofite_path.id_resource,
+        -- db_uoc_prod.stg_dadesra.dimax_resofite_path.ordre,
+    from db_uoc_prod.stg_dadesra.dimax_resofite_path  -- 18,056,913 vs 12,349,852
+
+)
+, node_structure_aplanation AS ( 
+    SELECT 
+        db_uoc_prod.stg_dadesra.dimax_item_dimax.cami_node,
+
+        db_uoc_prod.stg_dadesra.dimax_resofite_path.node_cami as id_resource,  -- id_ que equivale a la tabla de dim_catalog 
+        db_uoc_prod.stg_dadesra.dimax_resofite_path.node_cami,
+        db_uoc_prod.stg_dadesra.dimax_resofite_path.node_recurs, -- same as  db_uoc_prod.stg_dadesra.dimax_item_dimax.id 
+
+        db_uoc_prod.stg_dadesra.dimax_item_dimax.titol,
+        db_uoc_prod.stg_dadesra.dimax_v_recurs.id_recurs AS id_recurs2,
+        db_uoc_prod.stg_dadesra.dimax_v_recurs.titol AS titol_resource,
+        SUBSTR(db_uoc_prod.stg_dadesra.dimax_item_dimax.titol, 0, 6) AS DIM_ASSIGNATURA_KEY
  
     FROM db_uoc_prod.stg_dadesra.dimax_resofite_path    --- registros : 17,303,400
     
@@ -32,12 +41,10 @@ SELECT
 
     where 1=1
         and ( 
-            length( db_uoc_prod.stg_dadesra.dimax_item_dimax.cami_node ) >=  25
+            length( db_uoc_prod.stg_dadesra.dimax_item_dimax.cami_node ) >=  24
             or 
-            db_uoc_prod.stg_dadesra.dimax_item_dimax.titol like '%Root Node:PV%' --- eliminamos : Root Node:BIBLIO, usamos like pq hay muchos cami_node en null 
+            db_uoc_prod.stg_dadesra.dimax_item_dimax.titol like '%Root Node:PV%' --- eliminamos : Root Node:BIBLIO, usamos like pq hay muchos cami_node en null  --  ARRAY_SIZE(SPLIT(cami_node, ';'))  as length_resources-- 6,7,8,9,10,11,12
         )  
-        and db_uoc_prod.stg_dadesra.dimax_v_recurs.id_recurs =  69226
-        and db_uoc_prod.stg_dadesra.dimax_item_dimax.cami_node = ';3067899;3050333;3050332;'
 ) 
 
 , node_structure_resources  AS ( 
@@ -47,7 +54,7 @@ SELECT
         , NODE_CAMI  
         , NODE_RECURS
         , DIM_ASSIGNATURA_KEY AS nombre_resource   
- 
+        , ARRAY_SIZE(SPLIT(cami_node, ';'))  as length_resources
         , titol_resource  
         , SPLIT_PART(cami_node, ';', ARRAY_SIZE(SPLIT(cami_node, ';'))-1) AS NODE_RECURS_SEMESTRE
         , SPLIT_PART(cami_node, ';', ARRAY_SIZE(SPLIT(cami_node, ';'))-2) AS NODE_RECURS_intermedio
@@ -62,19 +69,22 @@ SELECT
 
 -- , node_structure_asignaturas AS ( 
     
-    SELECT    -- 275,315 
-        CAMI_NODE , count(*)
-        -- , NODE_RECURS  
-        -- , DIM_ASSIGNATURA_KEY  
-        -- , SPLIT_PART(CAMI_NODE, ';', ARRAY_SIZE(SPLIT(CAMI_NODE, ';'))-1) AS NODE_RECURS_SEMESTRE
-        -- , SPLIT_PART(CAMI_NODE, ';', ARRAY_SIZE(SPLIT(CAMI_NODE, ';'))-2) AS NODE_RECURS_intermedio
-        -- , SPLIT_PART(CAMI_NODE, ';', ARRAY_SIZE(SPLIT(CAMI_NODE, ';'))-3) AS NODE_RECURS_ASSIGNATURA
+    SELECT   
+  -- 275,315 
+        CAMI_NODE
+        , NODE_RECURS  
+        , DIM_ASSIGNATURA_KEY  
+        , ARRAY_SIZE(SPLIT(CAMI_NODE, ';'))  as length_resources
+        , titol
+        , SPLIT_PART(CAMI_NODE, ';', ARRAY_SIZE(SPLIT(CAMI_NODE, ';'))-1) AS NODE_RECURS_SEMESTRE
+        , SPLIT_PART(CAMI_NODE, ';', ARRAY_SIZE(SPLIT(CAMI_NODE, ';'))-2) AS NODE_RECURS_intermedio
+        , SPLIT_PART(CAMI_NODE, ';', ARRAY_SIZE(SPLIT(CAMI_NODE, ';'))-3) AS NODE_RECURS_ASSIGNATURA
  
     FROM node_structure_aplanation  -- 3M
     
-    where length (CAMI_NODE ) =  25 or length (CAMI_NODE ) =  24
-    group by 1 
-    order by 2 desc
+    where ( length (CAMI_NODE ) =  25 or length (CAMI_NODE ) =  24 ) 
+    and ARRAY_SIZE(SPLIT(CAMI_NODE, ';'))   in ( 6,7)
+ 
 
 ) 
 
@@ -110,7 +120,12 @@ SELECT  -- distinct 6,442,805 vs 6442805
 
 FROM node_structure_resources 
 
-    left join node_structure_asignaturas on right(node_structure_resources.cami_node, 25) = node_structure_asignaturas.cami_node  -- 6,442,805 ( base ) vs 5,893,628 --> perdemos 600K ( same distinct / no distinct )
+    left join node_structure_asignaturas 
+        on node_structure_resources.NODE_RECURS_SEMESTRE = node_structure_asignaturas.NODE_RECURS_SEMESTRE  -- 6,442,805 ( base ) vs 5,893,628 --> perdemos 600K ( same distinct / no distinct )
+            and node_structure_resources.NODE_RECURS_intermedio = node_structure_asignaturas.NODE_RECURS_intermedio 
+            and node_structure_resources.NODE_RECURS_ASSIGNATURA = node_structure_asignaturas.NODE_RECURS_ASSIGNATURA 
+
+
     -- inner join node_structure_semestres on  node_structure_resources.NODE_RECURS_SEMESTRE = node_structure_semestres.NODE_RECURS_SEMESTRE  -- 5,893,628 ( base ) vs 5,891,997 
     -- 1,6k La aplicación tinene 20 años y antes se usaba para otras cosas, está oboslteo lo que está bajo BIBLIO
     -- Hola, correcto el nodo BIBLIO es algo histórico y de cuando Diamx se usaba para otra función.... Ignoaramos el nodo Biblio.
